19e7f5b98572fdea6d82f37eee70948202f6b8d7
hevik <henktard@gmail.com>
2014-09-26 20:53:11 +0200

    remote

--- local/js/server.js
+++ local/js/server.js
@@ -1,8 +1,29 @@
 var express = require("express");
 var app = express();
-
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
 app.use(express.static("../remote"));
-app.listen(80);
+server.listen(80);
 
 console.log("serverUp");
+
+function makeId () {
+  return Math.random().toString(36).substr(2, 6);
+};
+
+io.on("connection", function(socket) {
+	var id = makeId();
+	console.log(id, "connected");
+
+	socket.on("jump", function() {
+		console.log(id, "jump")
+	})
+	socket.on("attack", function() {
+		console.log(id, "attacked")
+	})
+
+	socket.on("disconnect", function() {
+		console.log(id, "disconnect")
+	})
+});
