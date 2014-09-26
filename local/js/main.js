e9c894ddba7f00ed8d1d04d7570ccb166928fd55
Markus Nordangård <nordangaard@gmail.com>
2014-09-27 00:14:18 +0200

    Asd

--- local/js/main.js
+++ local/js/main.js
@@ -9,7 +9,7 @@ function preload() {
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	var hej = new Player();
+	var hej = new Player("AskiaSonghai");
 
 	hej.run();
 	hej.remove();
@@ -26,30 +26,4 @@ function makeId(){
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
-}
-
-
-var characters = {};
-
-function Player(){
-	this.id = makeId();
-
-	characters[this.id] = {};
-	characters[this.id]["sprite"] = {};
-
- 	characters[this.id]["sprite"] = game.add.sprite(0,0,'char');
-	characters[this.id]["sprite"].animations.add('run', [3,2,0,1], 8, true);
-}
-
-Player.prototype.run = function(){
-	characters[this.id]["sprite"].play("run");
-}
-
-
-Player.prototype.remove = function(){
-	delete characters[this.id]["sprite"];
-}
-
-Player.prototype.getId = function(){
-	return this.id;
 }
