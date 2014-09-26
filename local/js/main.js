295a97b17c7b129c0774bb87e2515e2dfb3db3c0
Markus Nordangård <nordangaard@gmail.com>
2014-09-27 00:09:02 +0200

    Asd

--- local/js/main.js
+++ local/js/main.js
@@ -38,13 +38,13 @@ function Player(){
 	characters[this.id]["sprite"] = {};
 
  	characters[this.id]["sprite"] = game.add.sprite(0,0,'char');
- 	console.log(characters[this.id]["sprite"]);
 	characters[this.id]["sprite"].animations.add('run', [3,2,0,1], 8, true);
 }
 
 Player.prototype.run = function(){
 	characters[this.id]["sprite"].play("run");
 }
+
 
 Player.prototype.remove = function(){
 	delete characters[this.id]["sprite"];
@@ -53,7 +53,3 @@ Player.prototype.remove = function(){
 Player.prototype.getId = function(){
 	return this.id;
 }
-
-
-
-//
