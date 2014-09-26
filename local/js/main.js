1517948c23a6865522af0b5318f154b09a060c95
Markus Nordangård <nordangaard@gmail.com>
2014-09-27 00:08:15 +0200

    Added stuuuuuuuff

--- local/js/main.js
+++ local/js/main.js
@@ -1,62 +1,59 @@
-var game = new Phaser.Game(500, 400, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', { preload: preload, create: create, update: update });
  
 function preload() {
 	game.load.spritesheet('char', 'assets/Run.png',200,200,4);
+	
+	
 }
  
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	//120,221
+	var hej = new Player();
 
-	// sprite.frame = 0; or any number up to 15 to access a specific frame
-
-	// or,
-
-	
-
-	// plays all the frames, at 1 frame per second, looping.
+	hej.run();
+	hej.remove();
 }
  
 function update() {
 }
 
-	
-var character = (function(){
-	var c = {}
-		sprite = {},
-		id = 0;
+function makeId(){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	var makeId = function(){
-	    var text = "";
-	    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-	    for( var i=0; i < 5; i++ )
-	        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-	    return text;
-	}
-
-	var animRun = function(){
-		sprite.play('ani');
-	}
-
-	c.create = function(){
-		sprite = game.add.sprite(0,0,'char');
-		sprite.animations.add('ani', [0,1,2,3], 8, true);
-
-		id = makeId();
-	}
-
-	c.run = function(){
-		animRun();
-	}
-
-	c.getId = function(){
-		return id;
-	}
-
-	return c;
-})();
+    return text;
+}
 
 
+var characters = {};
+
+function Player(){
+	this.id = makeId();
+
+	characters[this.id] = {};
+	characters[this.id]["sprite"] = {};
+
+ 	characters[this.id]["sprite"] = game.add.sprite(0,0,'char');
+ 	console.log(characters[this.id]["sprite"]);
+	characters[this.id]["sprite"].animations.add('run', [3,2,0,1], 8, true);
+}
+
+Player.prototype.run = function(){
+	characters[this.id]["sprite"].play("run");
+}
+
+Player.prototype.remove = function(){
+	delete characters[this.id]["sprite"];
+}
+
+Player.prototype.getId = function(){
+	return this.id;
+}
+
+
+
+//
