2a20ed700913f3bd90fcd6bd0d33e537d3447696
Markus Nordangård <nordangaard@gmail.com>
2014-09-26 21:56:38 +0200

    Added functionality to main.js to animate character

--- local/js/main.js
+++ local/js/main.js
@@ -1,20 +1,31 @@
-var game = new Phaser.Game(400, 300, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(500, 400, Phaser.AUTO, '', { preload: preload, create: create, update: update });
  
 function preload() {
+	game.load.spritesheet('char', 'assets/Run.png',200,200,4);
 }
  
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	//120,221
+
+	// sprite.frame = 0; or any number up to 15 to access a specific frame
+
+	// or,
+
+	
+
+	// plays all the frames, at 1 frame per second, looping.
 }
  
 function update() {
 }
 
-
+	
 var character = (function(){
-	var c = {};
-
-	var id = 0;
+	var c = {}
+		sprite = {},
+		id = 0;
 
 	var makeId = function(){
 	    var text = "";
@@ -26,8 +37,19 @@ var character = (function(){
 	    return text;
 	}
 
+	var animRun = function(){
+		sprite.play('ani');
+	}
+
 	c.create = function(){
+		sprite = game.add.sprite(0,0,'char');
+		sprite.animations.add('ani', [0,1,2,3], 8, true);
+
 		id = makeId();
+	}
+
+	c.run = function(){
+		animRun();
 	}
 
 	c.getId = function(){
@@ -36,3 +58,5 @@ var character = (function(){
 
 	return c;
 })();
+
+
