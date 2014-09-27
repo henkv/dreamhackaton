c72b10cf37a9fdc7d405bd40d18b85e2aae66d35
hevik <henktard@gmail.com>
2014-09-27 07:52:12 +0200

    jhdehdh

--- local/js/world.js
+++ local/js/world.js
@@ -6,6 +6,11 @@ var world = {
 		game.load.tilemap("map","assets/arena.json", null, Phaser.Tilemap.TILED_JSON)
 		game.load.image('GroundTileA', 'assets/GroundTileA.png');
 		game.load.image('GroundTileB', 'assets/GroundTileB.png');
+		game.load.image('GroundTileEnd1', 'assets/GroundTileEnd1.png');
+		game.load.image('GroundTileEnd2', 'assets/GroundTileEnd2.png');
+		game.load.image('GroundTileBigEnd1', 'assets/GroundTileBigEnd1.png');
+		game.load.image('GroundTileBigEnd2', 'assets/GroundTileBigEnd2.png');
+		
 		game.load.image('sky', "assets/Sky.png");	
 	},
 	create: function() {
@@ -16,6 +21,11 @@ var world = {
 		this.map = game.add.tilemap("map");
 		this.map.addTilesetImage("GroundTileA", "GroundTileA");
 		this.map.addTilesetImage("GroundTileB", "GroundTileB");
+		this.map.addTilesetImage("GroundTileEnd1", "GroundTileEnd1");
+		this.map.addTilesetImage("GroundTileEnd2", "GroundTileEnd2");
+		this.map.addTilesetImage("GroundTileBigEnd1", "GroundTileBigEnd1");
+		this.map.addTilesetImage("GroundTileBigEnd2", "GroundTileBigEnd2");
+
 
 		this.layers.pilars = this.map.createLayer("Pilars");
 		this.layers.ground = this.map.createLayer("Ground");
