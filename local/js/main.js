9ef30a4c19e5f81d39cfb26d277bb168389600b6
teodorvik <teovi698@student.liu.se>
2014-09-27 15:37:54 +0200

    Slau maj haurt i ansiktet

--- local/js/main.js
+++ local/js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 560, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1200, 860, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 var player, layers;
  
 function preload() {
@@ -7,6 +7,7 @@ function preload() {
 	game.load.spritesheet('charRunLeft', 'assets/Run_left.png',200,200,8);
 	game.load.spritesheet('charIdleLeft', 'assets/Idle.png',200,200,4);
 	
+	world.preload();
 }
  
 function create() {
@@ -26,4 +27,5 @@ function update() {
 }
 
 function render () {
+	world.render();
 }
--- local/js/world.js
+++ local/js/world.js
@@ -1,12 +1,15 @@
 var world = {};
  
 world.preload = function() {
-	game.load.tilemap("map","assets/arena.json", null, Phaser.Tilemap.JSON)
+	game.load.tilemap("map","assets/arena.json", null, Phaser.Tilemap.TILED_JSON)
 	game.load.image('tiles', 'assets/AllGroundTiles.png');
 	game.load.image('sky', "assets/Sky.png");	
+
+
 }
 
 var map;
+var tileset;
 var bg;
 var layer;
 var cursors;
@@ -15,20 +18,24 @@ var player;
 world.create = function() {
 	bg = game.add.sprite(0,0,"sky");
 
-	map = game.add.tilemap('map', 22, 8);
+	map = game.add.tilemap('map');
 
 	map.addTilesetImage("AllGroundTiles", "tiles");
+
+	map.setCollisionBetween(0, 10);
 
 	layer = map.createLayer(0);
 
 	layer.resizeWorld();
-
-	map.setCollisionBetween(0, 10);
 
 }
 
 world.update = function(){
 	
 
+}
+
+world.render = function() {
+	game.debug.body(map);
 }
 
