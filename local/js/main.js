11aa5df2e28413ddb87b230626fbc159fc38057b
hevik <henktard@gmail.com>
2014-09-27 14:36:24 +0200

    Newage Player

--- local/js/main.js
+++ local/js/main.js
@@ -1,5 +1,5 @@
-var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-var players, layers;
+var game = new Phaser.Game(800, 560, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+var player, layers;
  
 function preload() {
 	
@@ -12,53 +12,18 @@ function preload() {
  
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	
-	createServer();
-	//game.physics.setImpactEvents(true);
-	//game.physics.arcade.collide('char', sprite2, collisionHandler, null, this);
 	game.physics.arcade.gravity.y = 2000;
 
-	layers = game.add.group();
-	world.create();
-	game.physics.enable
+	player = makePlayer();
+	createServer();
 
-	// hej.move();
 
-	players = game.add.group();
-	players.setAll('body.collideWorldBounds', true);
-	players.setAll('body.bounce.x', 4);
-	players.setAll('body.bounce.y', 2);
-
-	var ll = {};
-	for(var i = 0; i<100; i++){
-		ll[i] = new Player(i, "Chefren"+i);
-	}
 
 }
  
 function update() {
-
-	game.physics.arcade.collide(players, layers);
-
-	for(c in characters){
-		var x = characters[c]["sprite"].position.x;
-		var y = characters[c]["sprite"].position.y;
-		characters[c]["x"] = x;
-		characters[c]["y"] = y;
-
-		characters[c]["name"].position.x = (x+105);
-		characters[c]["name"].position.y = y-20;
-		friction(c);
-
-	}
-
-
-	world.update();
+	game.physics.arcade.collide(player);//, layer);	
 }
 
 function render () {
-	for(c in characters){ 
-		game.debug.body(characters[c].sprite);
-
 	}
-}
