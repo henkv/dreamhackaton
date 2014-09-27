9ae4c82026e7a631302b6359dd18da52421b18d9
hevik <henktard@gmail.com>
2014-09-27 08:28:30 +0200

    as

--- local/js/player.js
+++ local/js/player.js
@@ -101,7 +101,7 @@ Player.prototype.smash = function(){
 	if(this.facing){
 
 		var DZS = x + inset + body;
-		var DZE = x + inset * 1.1 + body;
+		var DZE = x + body;
 		
 		for(c in characters){
 			var val = characters[c];
