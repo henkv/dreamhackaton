c1f5c29c89a152ac207408b2ed0f31e5b3fec3bf
hevik <henktard@gmail.com>
2014-09-27 09:51:05 +0200

    gh

--- local/js/player.js
+++ local/js/player.js
@@ -143,13 +143,13 @@ Player.prototype.getId = function(){
 }
 
 
-var friction = function(sprite) {
-	//var sprite = characters[id]["sprite"];
+var friction = function(id) {
+	var sprite = characters[id]["sprite"];
 	var vel = sprite.body.velocity;
 	if (vel.y === 0 && vel.x > 0) {
-		characters[this.id].sprite.body.velocity.x -= 1;
+		characters[id].sprite.body.velocity.x -= 1;
 	} else if (vel.y === 0 && vel.x < 0) {
-		characters[this.id].sprite.body.velocity.x += 1;
+		characters[id].sprite.body.velocity.x += 1;
 	}
 
 }
